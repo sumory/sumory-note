c45a5655eaebfba7cfb4d53d8f37ea64617b37e9
root <root@asicme.(none)>
2013-10-14 16:16:34 +0800

    rename analyze result files

--- getblocks/cal_block.sh
+++ getblocks/cal_block.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-rm -rf date_block.log
-rm -rf blocks.log
-rm -rf sort_date_block.log
+rm -rf date_block.result
+rm -rf blocks.result
+rm -rf sort_date_block.result
 
 awk -F '\t' '{
     split($1, array, " ");
     height = array[3];
 
     print height"\t"$2"\t"$3;
-}' logs/block.log > blocks.log 
+}' logs/block.log > blocks.result 
 
 awk -F '\t' '{
     height = $1;
@@ -29,7 +29,7 @@ END{
         print c"\t"count[c]
     }
 
-}' blocks.log > date_block.log
+}' blocks.result > date_block.result
 
-sort -k 1 -r date_block.log > sort_date_block.log
-rm -rf date_block.log
+sort -k 1 -r date_block.result > sort_date_block.result
+rm -rf date_block.result
