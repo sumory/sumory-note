88e3a918965b0ba8bd2157a5f941ca63085623d6
root <root@asicme.(none)>
2013-11-08 19:28:01 +0800

    add functions to bitcoin_rpc.py

--- asicme-pool/lib/bitcoin_rpc.py
+++ asicme-pool/lib/bitcoin_rpc.py
@@ -4,8 +4,8 @@
 
 import json
 import base64
-import geventhttpclient
 import logger
+import geventhttpclient
 
 class BitcoinRPC(object):
     
@@ -29,27 +29,62 @@ class BitcoinRPC(object):
                 'method': method,
                 'params': params,
                 'id': '1',
-            }))
+        }))
 
-    def submitblock(self, block_hex):
-        resp = self._call('submitblock', [block_hex])
-        if json.loads(resp)['result'] == None:
-            return True
+    def _pay_call_raw(self, data):
+        logger.log('pay', 'req:', data)
+        res = self.client.post('/', body=data, headers=self.headers).read()
+        logger.log('pay', 'res:', res)
+        return res
+
+    def _pay_call(self, method, params):
+        return self._pay_call_raw(json.dumps({
+            'jsonrpc': '2.0',
+            'method': method,
+            'params': params,
+            'id': '1',
+        }))
+
+                                           
+    def validateaddress(self, address):
+        resp = self._pay_call('validateaddress', [address])
+        return json.loads(resp)['result']['isvalid']
+
+
+    def settxfee(self, amount):
+        resp = self._call('settxfee', [amount])
+        return resp
+
+    def walletpassphrase(self, passphrase, timeout):
+        resp = self._call('walletpassphrase', [passphrase, timeout])
+        return resp
+
+    def sendtoaddress(self, address, amount):
+        resp = self._call('sendtoaddress', [address, amount])
+        return json.loads(resp)
+
+    def getbalance(self, account):
+        resp = self._pay_call('getbalance', [account])
+        result = json.loads(resp)
+        if(result['error']):
+            logger.log('error','getbalance error %s' % result)
+            return 0
         else:
-            return False
+            return result['result']
 
-    def getblocktemplate(self):
-        resp = self._call('getblocktemplate', [])
+    def sendfrom(self, account, to_address, amount):
+        resp = self._pay_call('sendfrom', [account, to_address, amount])
+        result = json.loads(resp)
+        if(result['error']):
+            logger.log('error','sendfrom error %s' % result)
+            return None
+        else:
+            return result['result']
+
+    def getblock(self, blockhash):
+        resp = self._call('getblock', [blockhash])
         return json.loads(resp)['result']
 
-    def prevhash(self):
-        resp = self._call('getwork', [])
-        try:
-            return json.loads(resp)['result']['data'][8:72]
-        except Exception as e:
-            logger.log('error', "Cannot decode prevhash %s" % str(e))
-            raise
-                                                  
-    def validateaddress(self, address):
-        resp = self._call('validateaddress', [address])
+    def gettxout(self, txid, n):
+        resp = self._call('gettxout', [txid, n])
         return json.loads(resp)['result']
